be4826feded926e255a032c6b966fc0e7fdeb8aa
PavanYalamanchi <pavan.yalamanchi@gmail.com>
2021-06-03 23:54:50 +0100

    Final teams application

--- teams.js
+++ teams.js
@@ -1,15 +1,30 @@
 function submitForm() {
-    let teamNumer = document.getElementById('exampleInputEmail1').value
-    let userNames = document.getElementById('exampleInputPassword1').value
-    let teamSize = document.getElementById('exampleInputSize').value
+    let teamNumber = document.getElementById('inputGroupSelect01').value
+    let teamSize = document.getElementById('inputGroupSelect02').value
 
-    let arrayOfUsernames = userNames.split(',')
-    let teamArray = []
-    if (typeof teamNumber === 'number' && arrayOfUsernames.length >= 1) {
-        for (let j = 0; j < teamNumber; j++) {
-            for (let i = 0; i < teamSize; i++) {
-                teamArray[i].push(arrayOfUsernames.pop())
-            }
+    let availUsers = []
+    let teamMembers = []
+    for (let i = 1; i <= (teamSize * teamNumber); i++) {
+        availUsers.push(`user${i}`)
+    }
+    for (let j = 1; j <= teamNumber; j++) {
+        let form = document.getElementById('form')
+        let divRow = document.createElement('div')
+        let teamContent = document.createElement('h4')
+        teamContent.setAttribute('class', 'text-white')
+        divRow.setAttribute('class', 'row')
+        form.appendChild(divRow)
+        let divCol = document.createElement('div')
+        divCol.setAttribute('class', 'col')
+        divRow.appendChild(divCol)
+        let teamName = document.createElement('h4')
+        teamName.innerHTML = `Team${j}<br>`
+        divCol.appendChild(teamName)
+        teamMembers.length = 0
+        for (let k = 1; k <= teamSize; k++) {
+            teamMembers.push(availUsers.pop())
+            teamContent.innerHTML = teamMembers
+            divCol.appendChild(teamContent)
         }
     }
 }
